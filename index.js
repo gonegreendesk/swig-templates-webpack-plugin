3847cd420a8f5a7a1a33f5ff0c1fb66ef73866f1
cyberwombat <yashua@urbanlotus.com>
2023-08-21 10:16:05 -0700

    Cleanup

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const beautify = require('js-beautify').html
 const uglify = require('html-minifier')
 const swig = require('swig-templates')
 let swigOptions = {
-  cache: false
+  cache: false,
 }
 
-function SwigWebpackPlugin (options) {
+function SwigWebpackPlugin(options) {
   this.options = options || {}
   this.options.sds = 's'
 }
 
 SwigWebpackPlugin.prototype.apply = function (compiler) {
-  compiler.plugin('emit', (compiler, callback) => {
+  compiler.hooks.emit.tap('SwigPlugin', (compiler, callback) => {
     const webpackStatsJson = compiler.getStats().toJson()
     let templateParams = {}
     templateParams.webpack = webpackStatsJson
     templateParams.swigWebpackPlugin = {}
-    templateParams.swigWebpackPlugin.assets = this.swigWebpackPluginAssets(compiler, webpackStatsJson)
+    templateParams.swigWebpackPlugin.assets = this.swigWebpackPluginAssets(
+      compiler,
+      webpackStatsJson
+    )
     templateParams.swigWebpackPlugin.options = this.options
     templateParams.locals = this.options.data || null
 
@@ -28,24 +31,39 @@ SwigWebpackPlugin.prototype.apply = function (compiler) {
     const templateFile = this.options.template
     const templateContext = context
 
-    const files = glob.sync(templateFile, {root: templateContext, realpath: true})
+    const files = glob.sync(templateFile, {
+      root: templateContext,
+      realpath: true,
+    })
     if (files.length > 0) {
-      files.forEach(template => {
+      files.forEach((template) => {
         const data = fs.readFileSync(template, 'utf8')
         if (data) {
           this.emitHtml(compiler, template, templateParams, outputFilename)
         } else {
-          compiler.errors.push(new Error('SwigWebpackPlugin: Unable to read HTML template "' + template + '"'))
+          compiler.errors.push(
+            new Error(
+              'SwigWebpackPlugin: Unable to read HTML template "' +
+                template +
+                '"'
+            )
+          )
         }
       })
     } else {
       compiler.errors.push(new Error('SwigWebpackPlugin: Unable to read files'))
     }
-    callback()
+    return true
+    //callback()
   })
 }
 
-SwigWebpackPlugin.prototype.emitHtml = function (compiler, htmlTemplateFile, templateParams, outputFilename) {
+SwigWebpackPlugin.prototype.emitHtml = function (
+  compiler,
+  htmlTemplateFile,
+  templateParams,
+  outputFilename
+) {
   swig.setDefaults(swigOptions)
 
   const template = swig.compileFile(htmlTemplateFile, templateParams)
@@ -58,32 +76,36 @@ SwigWebpackPlugin.prototype.emitHtml = function (compiler, htmlTemplateFile, tem
     },
     size: () => {
       return html.length
-    }
+    },
   }
 }
 
 SwigWebpackPlugin.prototype.htmlFormatter = function (options, html) {
   if (options.beautify) {
     return beautify(html, {
-      indentSize: 2
+      indentSize: 2,
     })
   } else if (options.uglify) {
     return uglify.minify(String(html), {
-      collapseWhitespace: true
+      collapseWhitespace: true,
     })
   } else {
     return html
   }
 }
 
-SwigWebpackPlugin.prototype.swigWebpackPluginAssets = function (compiler, webpackStatsJson) {
+SwigWebpackPlugin.prototype.swigWebpackPluginAssets = function (
+  compiler,
+  webpackStatsJson
+) {
   let assets = {
-    extensions: {}
+    extensions: {},
   }
 
   for (const chunk in webpackStatsJson.assetsByChunkName) {
-    let chunkFiles = [].concat(webpackStatsJson.assetsByChunkName[chunk])
-      .map(fileName => {
+    let chunkFiles = []
+      .concat(webpackStatsJson.assetsByChunkName[chunk])
+      .map((fileName) => {
         if (compiler.options.output.publicPath) {
           return compiler.options.output.publicPath + fileName
         }
@@ -93,7 +115,7 @@ SwigWebpackPlugin.prototype.swigWebpackPluginAssets = function (compiler, webpac
 
     assets[chunk] = chunkFiles[0]
 
-    chunkFiles.forEach(chunkFile => {
+    chunkFiles.forEach((chunkFile) => {
       const ext = chunkFile.split('.').pop()
 
       assets.extensions[ext] = assets.extensions[ext] || []
